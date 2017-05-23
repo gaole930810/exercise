817c8edcfed002c9548f97d3842b1072894d03fc
gaole <gaole@DESKTOP-BUSM6QI.lan>
2017-05-23 15:03:46 +0800

    gaole qinshi 15:03

--- src/leetcode/medium/Q3.java
+++ src/leetcode/medium/Q3.java
@@ -11,8 +11,10 @@ public class Q3 {
 			String subStr="";
 			for(;locat<charArray.length;locat++){
 				String c=String.valueOf(charArray[locat]);
-				if(subStr.contains(c))
+				if(subStr.contains(c)){
+					start+=subStr.indexOf(c);
 					break;
+				}					
 				subStr+=c;
 			}
 			if(subStr.length()>maxL){
