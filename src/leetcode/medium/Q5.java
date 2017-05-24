4bdf862b729ee6e66cd2f6dbbbcb48a97b6b263f
le <le@gaole>
2017-05-24 12:09:34 +0800

    gaole shiyanshi 12:09

--- src/leetcode/medium/Q5.java
+++ src/leetcode/medium/Q5.java
@@ -1,45 +1,23 @@
 package leetcode.medium;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Q5 {
 	public String longestPalindrome(String s) {
-		int maxL=0;
-		List<Character> l=new ArrayList<Character>();
-		String maxSubStr="";
-		char [] charArray=s.toCharArray();
-		for(int start=0;start<charArray.length&&charArray.length-start>maxL;start++){
-			if(!l.contains(s.charAt(start))){
-				l.add(s.charAt(start));
-				int subStrL=s.lastIndexOf(s.charAt(start))-start+1;
-				if(subStrL>maxL){
-					maxL=subStrL;
-					maxSubStr=s.substring(start,start+subStrL);
-				}
+		if(s.length()==1||s.length()==0)
+			return s;
+		else{
+			if(s.charAt(0)==s.charAt(s.length()-1)&&longestPalindrome(s.substring(1,s.length()-1)).length()==s.length()-2){
+				return s;
+			}
+			else{
+				String subL=longestPalindrome(s.substring(0,s.length()-1));
+				String subR=longestPalindrome(s.substring(1,s.length()));
+				return subL.length()>=subR.length()?subL:subR;
 			}
 		}
-		
-		for(int start=0;start<charArray.length&&charArray.length-start>maxL;start++){
-			int locat=start;
-			String subStr="";
-			for(;locat<charArray.length;locat++){
-				String c=String.valueOf(charArray[locat]);
-				if(subStr.contains(c)){
-					start+=subStr.indexOf(c);
-					subStr=s.substring(start, locat+1);
-					break;
-				}					
-				subStr+=c;
-			}
-			if(subStr.length()>maxL){
-				maxL=subStr.length();
-				maxSubStr=subStr;
-			}
-			
-		}		
-		return maxSubStr;
     }
-	
+	/**
+	 * 
+	 */
 
 }
--- src/leetcode/medium/Q5test.java
+++ src/leetcode/medium/Q5test.java
@@ -2,7 +2,7 @@ package leetcode.medium;
 
 public class Q5test {
 	public static void main(String[] args){
-		System.out.println(new Q5().longestPalindrome("ccc"));
+		System.out.println(new Q5().longestPalindrome("babaddtattarrattatddetartrateedredividerb"));
 	}
 
 }
